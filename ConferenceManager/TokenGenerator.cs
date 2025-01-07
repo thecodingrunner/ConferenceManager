ac7abd700d824876d34c50f57c0988f603639b9e
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-01-07 12:03:26 +0000

    completed task 5

---
+++ ConferenceManager/TokenGenerator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace ConferenceManager
+{
+    public class TokenGenerator
+    {
+        public void Generate()
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your-very-secure-secret-which-should-be-quite-long"));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: "your-name",
+                audience: "your-app-name",
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials
+            );
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // Turn the token object into the token string which is added to the header
+            string jwtToken = tokenHandler.WriteToken(token);
+
+            // This prints to the debug output in VS2022
+            System.Diagnostics.Debug.WriteLine(jwtToken);
+        }
+    }
+}
